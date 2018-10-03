de0f6a2e3e5ee5b845efbb16abea43592a069285
Daniel Williamson <williamson_daniel@columbusstate.edu>
2018-10-03 20:03:41 +0000

    Lab 7

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php 
 
-$page_title = '';
+// for adding ads to a Web page.
+function create_ad() {
+  $ad = "  Lab 7!  ";
+  echo '<div class="alert alert-info" role="alert">';
+  for ($i=0; $i<10; $i++){
+    echo ($ad);
+  }
+  echo '</div>';
+} // End of the function definition.
+
+$page_title = 'Welcome to Lab 7!';
 include('includes/header.html');
 
+// Call the ad function
+create_ad();
 ?>
 
-<div class="page-header"><h1></h1></div>
-
+<div class="page-header"><h1>Lab 7: Midterm Practice</h1></div>
+<p>This is a practice website for the CPSC4125 midterm exam.</p>
 
 <?php
+// Call the ad function again
+create_ad();
 
-
-include('../includes/footer.html');
+include('includes/footer.html');
 ?>
